0f6dc22d9d910ce5c05dbc0fbfe32e5ed03a15bc
Alex Schwartz <alexschwartz01@gmail.com>
2023-11-20 07:32:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require('http')
 
-module.exports = function (port) {
+module.exports = function () {
     const server = http.createServer((req, res) => {
         res.end("<body>Hello World</body>")
     })
 
-    server.listen(port, 'localhost', () => {
+    server.listen(8080, 'localhost', () => {
         console.log(`Listening on http://localhost:${server.address().port}`)
     })
 }
